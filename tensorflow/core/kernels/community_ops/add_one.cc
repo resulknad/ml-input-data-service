1104494835f6e5aab10c43728e9ac1d29ea1b7d9
Damien Aymon <damien.aymon@inf.ethz.ch>
2020-10-12 18:21:55 +0200

    removed BUILD file from /core/user_ops

--- tensorflow/core/kernels/community_ops/add_one.cc
+++ tensorflow/core/kernels/community_ops/add_one.cc
@@ -10,6 +10,11 @@ class AddOneOp : public OpKernel {
     void Compute(OpKernelContext* context) override {
       // Get the input
       const Tensor& input = context->input(0);
+
+      //Changing then implementation for the sake of rebuilding the thing..
+      const Tensor& input2 = context->input(0);
+
+
       auto flat_input = input.flat<int32>();
 
       // Create the output and alocate memory
