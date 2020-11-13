f574d30cc2b508af260103bdeb14ad5c0acf9f13
Damien Aymon <aymond@ethz.ch>
2020-11-13 16:59:11 +0100

    bug fix

--- tensorflow/core/data/service/dispatcher_impl.cc
+++ tensorflow/core/data/service/dispatcher_impl.cc
@@ -357,12 +357,12 @@ Status DataServiceDispatcherImpl::MakeSplitProvider(
 Status DataServiceDispatcherImpl::UpdateMetadata(
     const UpdateMetadataRequest* request,
     UpdateMetadataResponse* response) EXCLUSIVE_LOCKS_REQUIRED(mu_) {
-  uint64 fingerprint = request->fingerprint();
+  uint64 fingerprint = request->id();
   int64 update = request->update();
   // TODO(damien-aymon) check if started?
   mutex_lock l(mu_);
-  TF_RETURN_IF_ERROR( metadata_store_.UpdateMetadata(fingerprint, update));
-  VLOG3() << "Updated metadata for fingerprint " << fingerprint;
+  TF_RETURN_IF_ERROR(metadata_store_.UpdateMetadata(fingerprint, update));
+  VLOG(3) << "Updated metadata for fingerprint " << fingerprint;
   return Status::OK();
 }
 
