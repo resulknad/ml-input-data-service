8d2d8a76dc1e35fb51af3f437fc187c583c3f1fa
Damien Aymon <aymond@fdr1.inf.ethz.ch>
2021-04-23 12:22:45 +0200

    some more debugging

--- tensorflow/core/data/standalone.cc
+++ tensorflow/core/data/standalone.cc
@@ -41,9 +41,14 @@ namespace standalone {
 
 Status Iterator::GetNext(std::vector<Tensor>* outputs, bool* end_of_input) {
   monitoring::CounterCell* tf_data_elements_counter =
-      GetTFDataElementsCounter("ServiceCacheGet");
+      tensorflow::metrics::GetTFDataElementsCounter("ServiceCacheGet");
   VLOG(0) << " EASL - serviceCacheGet elements counter: " <<
   tf_data_elements_counter->value();
+  auto model = ctx_.get()->model();
+
+  if(model){
+    VLOG(0) << "EASL - serviceCacheGet: there is indeed a model here...";
+  }
   return iterator_->GetNext(ctx_.get(), outputs, end_of_input);
 }
 
