3f0cd6e5a65beb68a09eb821ff86f19265e3f109
Damien Aymon <damien.aymon@inf.ethz.ch>
2021-06-10 14:16:20 +0200

    comment added for client hb interval

--- tensorflow/core/kernels/data/experimental/data_service_dataset_op.cc
+++ tensorflow/core/kernels/data/experimental/data_service_dataset_op.cc
@@ -71,7 +71,7 @@ namespace data {
 
 namespace {
 // Default interval between task list refreshes.
-const int64 kDefaultTaskRefreshIntervalMs = 50;  // 1 second.
+const int64 kDefaultTaskRefreshIntervalMs = 50;  // 1 second. => now 50msec.
 
 constexpr char kDataServiceDatasetV1[] = "DataServiceDataset";
 constexpr char kDataServiceDatasetV2[] = "DataServiceDatasetV2";
