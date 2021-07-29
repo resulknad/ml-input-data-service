51391da42a39b0e7bd9d968167336805d9809b63
Damien Aymon <damien.aymon@inf.ethz.ch>
2021-07-29 12:56:00 +0200

    bug fix

--- tensorflow/core/data/service/worker_impl.cc
+++ tensorflow/core/data/service/worker_impl.cc
@@ -183,7 +183,6 @@ Status DataServiceWorkerImpl::GetElementResult(
       } else {
         // Perhaps the workers hasn't gotten the task from the dispatcher yet.
         // Return Unavailable so that the client knows to continue retrying.
-        // TODO (dada) remove vlog
         VLOG(0) << "Task not found (probably not received from dispatcher yet";
         return errors::Unavailable("Task ", request->task_id(), " not found");
       }
@@ -201,8 +200,7 @@ Status DataServiceWorkerImpl::GetElementResult(
 
   if (result->end_of_sequence) {
     mutex_lock l(mu_);
-    // TODO (dada) - revert to 3
-    VLOG(0) << "Reached end_of_sequence for task " << request->task_id();
+    VLOG(3) << "Reached end_of_sequence for task " << request->task_id();
     pending_completed_tasks_.insert(request->task_id());
     task_completion_cv_.notify_one();
   }
@@ -248,8 +246,7 @@ Status DataServiceWorkerImpl::EnsureTaskInitialized(
       config_, task.task_def, std::move(task_iterator), task.task_runner));
 
   task.initialized = true;
-  // TODO (dada) revert to 3
-  VLOG(0) << "Created iterator for task " << task.task_def.task_id();
+  VLOG(3) << "Created iterator for task " << task.task_def.task_id();
   return Status::OK();
 }
 
@@ -326,20 +323,15 @@ void DataServiceWorkerImpl::StopTask(Task& task) TF_LOCKS_EXCLUDED(mu_) {
 
 Status DataServiceWorkerImpl::GetElement(const GetElementRequest* request,
                                          GetElementResponse* response) {
-  // TODO (dada) revert to 3
-  VLOG(0) << "Received GetElement request for task " << request->task_id();
+  VLOG(3) << "Received GetElement request for task " << request->task_id();
   struct GetElementResult result;
   TF_RETURN_IF_ERROR(GetElementResult(request, &result));
   response->set_end_of_sequence(result.end_of_sequence);
   response->set_skip_task(result.skip);
   if (!response->end_of_sequence() && !response->skip_task()) {
-    VLOG(0) << "moving element to response";
     TF_RETURN_IF_ERROR(
         MoveElementToResponse(std::move(result.components), *response));
-    // TODO (dada) revert to 3
-    VLOG(0) << "Producing an element for task " << request->task_id();
-  } else {
-    VLOG(0) << "not moving element..";
+    VLOG(3) << "Producing an element for task " << request->task_id();
   }
   return Status::OK();
 }
