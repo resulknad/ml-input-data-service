35dbf37876b943ca19020619e40add105470704e
Muyu Li <muyuli@student.ethz.ch>
2021-12-30 14:06:07 +0100

    Fix re-assign worker bug

--- tensorflow/core/data/service/dispatcher_state.cc
+++ tensorflow/core/data/service/dispatcher_state.cc
@@ -461,10 +461,12 @@ void DispatcherState::ReassignFreeWorkers() {
     // Assign one worker to the job
     workers_by_job_[job->job_id].push_back(it->second);
     jobs_by_worker_[it->second->address][job->job_id] = jobs_[job->job_id];
-    avail_workers_.erase(it);
+
 
     VLOG(0) << "EASL - (ReassignFreeWorkers) Reassigned worker "
     << it->second->address << " to job " << job->job_id;
+
+    avail_workers_.erase(it++);
   }
 }
 
