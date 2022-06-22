2b23bd37c4e7858553d0217675577b8500b578f6
Zak Cook <zakcook4@gmail.com>
2022-06-22 11:49:03 +0200

    Reorder result queue

--- tensorflow/core/kernels/data/experimental/data_service_dataset_op.cc
+++ tensorflow/core/kernels/data/experimental/data_service_dataset_op.cc
@@ -1612,6 +1612,13 @@ class DataServiceDatasetOp::Dataset : public DatasetBase {
       const bool local_result_ready =
           !local_results_buffer_.empty() && local_results_buffer_.front().ready;
       const bool result_ready = !results_.empty() && results_.front().ready;
+      if (!results_.front().task_id == processed_task_ids_.front()) {
+        for (auto result: results_) {
+          if (result_.task_id == processed_task_ids_.front()) {
+            std::swap(results_.front(), result);
+          }
+        }
+      }
       return local_result_ready || result_ready;
     }
 
