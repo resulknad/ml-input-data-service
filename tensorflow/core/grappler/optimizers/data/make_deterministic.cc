96d46cde4a33dde13945ecebda13f1369cbea866
Zak Cook <zakcook4@gmail.com>
2022-06-24 11:12:15 +0200

    Rollback changes in make_deterministic.cc

--- tensorflow/core/grappler/optimizers/data/make_deterministic.cc
+++ tensorflow/core/grappler/optimizers/data/make_deterministic.cc
@@ -485,22 +485,20 @@ Status MakeDeterministic::OptimizeAndCollectStats(Cluster* cluster,
             << ") because it introduces nondeterminism through "
             << (rewrite_due_to_async ? "asynchrony" : "parallelism");
 
-    // bool num_changes = true;
-    // if (IsPrefetch(node.op())) {
-    //   TF_RETURN_IF_ERROR(ConvertPrefetch(node.name(), &graph));
-    // } else if (IsMapAndBatch(node.op())) {
-    //   TF_RETURN_IF_ERROR(ConvertMapAndBatch(node.name(), &graph));
-    //   nodes_to_delete.insert(node.name());
-    // } else if (IsParallelBatch(node.op())) {
-    //   TF_RETURN_IF_ERROR(ConvertBatch(node.name(), &graph));
-    // } else if (IsParallelInterleave(node.op())) {
-    //   TF_RETURN_IF_ERROR(ConvertMapOrInterleave(node.name(), &graph));
-    // } else {
-    //   DCHECK(IsParallelMap(node.op()));
-    //   // TF_RETURN_IF_ERROR(ConvertMapOrInterleave(node.name(), &graph));
-    //   num_changes = false;
-    // }
-    // stats->num_changes += num_changes;
+    if (IsPrefetch(node.op())) {
+      TF_RETURN_IF_ERROR(ConvertPrefetch(node.name(), &graph));
+    } else if (IsMapAndBatch(node.op())) {
+      TF_RETURN_IF_ERROR(ConvertMapAndBatch(node.name(), &graph));
+      nodes_to_delete.insert(node.name());
+    } else if (IsParallelBatch(node.op())) {
+      TF_RETURN_IF_ERROR(ConvertBatch(node.name(), &graph));
+    } else if (IsParallelInterleave(node.op())) {
+      TF_RETURN_IF_ERROR(ConvertMapOrInterleave(node.name(), &graph));
+    } else {
+      DCHECK(IsParallelMap(node.op()));
+      TF_RETURN_IF_ERROR(ConvertMapOrInterleave(node.name(), &graph));
+    }
+    stats->num_changes++;
   }
 
   TF_RETURN_IF_ERROR(graph.DeleteNodes(nodes_to_delete));
